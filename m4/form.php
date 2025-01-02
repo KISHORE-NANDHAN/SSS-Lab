d85b3d54dc31aac86382d408175d34a2345d68ba
kishore <nandhakishorejajula@gmail.com>
2025-01-02 14:34:11 +0530

    m4

---
+++ m4/form.php
@@ -0,0 +1,6 @@
+<?php 
+
+    echo "The User Name is ". $_REQUEST["username"];  // here for GET in index we have to use $GET and POST we have use $POST
+    echo "<br/>The password is ". $_REQUEST["password"];  // if we don't know what it is then we can use $REQUEST
+
+?>
