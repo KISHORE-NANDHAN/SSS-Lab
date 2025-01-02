cddbaec4ddc64f8a9aa26f08e4d47f6a0316000d
kishore <nandhakishorejajula@gmail.com>
2025-01-02 15:54:52 +0530

    new changes in module 4

--- m4/form.php
+++ m4/form.php
@@ -1,5 +1,5 @@
 <?php 
-
+    include 'links.html';
     echo "The User Name is ". $_REQUEST["username"];  // here for GET in index we have to use $GET and POST we have use $POST
     echo "<br/>The password is ". $_REQUEST["password"];  // if we don't know what it is then we can use $REQUEST
 
